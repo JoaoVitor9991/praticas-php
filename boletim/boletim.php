fa44cd97294a303e2c0f207943f7343d4f4b3b46
JoaoVitor9991 <joaovitormachadodev99@gmail.com>
2025-12-13 16:29:17 -0400

    att

---
+++ boletim/boletim.php
@@ -0,0 +1,13 @@
+<?php 
+
+Class Aluno{
+    private $nome;
+    private $notas = [];
+
+    public function __construct($nome_que_chegou)
+    {
+        $this->nome = $nome_que_chegou;
+    }
+
+    public function 
+}
