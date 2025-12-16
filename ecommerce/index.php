17b5079c5343e7f8e1bd09c67181564ede3cfd51
JoaoVitor9991 <joaovitormachadodev99@gmail.com>
2025-12-15 21:09:59 -0400

    att

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -5,3 +5,10 @@ require_once 'produto.php';
 
 $produto1 = new Produto('Mouse', 1000);
 $produto2 = new Produto('Teclado', 1500);
+
+$carrinho = new Carrinho();
+
+$carrinho->adicionar($produto1);
+$carrinho->adicionar($produto2);
+
+echo "Total da compra: R$ " . $carrinho->calcular_total();
